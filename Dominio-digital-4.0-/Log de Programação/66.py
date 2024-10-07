c0613091ef8670b309450f3f0cbd1b276f601e82
Wallace-Modesto <Wallace_15_modesto@hotmail.com>
2024-10-07 11:49:38 -0300

    Add files via upload

---
+++ Dominio-digital-4.0-/Log de Programação/66.py
@@ -0,0 +1,11 @@
+a = int(input("Digite o primeiro numero: "))
+b = int(input("Digite o segundo numero: "))
+
+if b == a:
+    print("Esses números são iguais")
+else:
+    if b > a:
+        print(a,b)
+    else:
+        print(b,a)
+
